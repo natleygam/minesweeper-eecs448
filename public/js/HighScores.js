d777cbf93f7afb68bf4154f64a65ba7d369c3cef
EvanBrown96 <evan.br96@yahoo.com>
2018-09-10 22:27:42 -0500

    add initial highscore files

---
+++ public/js/HighScores.js
@@ -0,0 +1,24 @@
+class HighScoreObject{
+  constructor(){
+    this.test = "hi";
+  }
+
+  thing(){
+    console.log(this.test);
+  }
+}
+
+class HighScoreManager{
+
+  constructor(){
+    this.myjson_id = "rw7kg";
+  }
+
+  addScore(rows, cols, time, percent){
+
+  }
+
+  getScores(){
+    
+  }
+}
